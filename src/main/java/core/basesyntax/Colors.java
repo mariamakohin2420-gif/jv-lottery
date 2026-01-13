235d0f807766aac37fc5735aa701d005468c5292
Mariia Makohin <maria.makohin2420@gmail.com>
2026-01-13 21:04:15 +0200

    first solutionchanges

--- src/main/java/core/basesyntax/Colors.java
+++
@@ -1,5 +0,0 @@
-package core.basesyntax;
-
-public enum Colors {
-    String colorblue = new String blue;
-}
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+import java.util.Random;
+public class Lottery {
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+
+    public Ball getRandomBall(){
+
+        Color randomColor = colorSupplier.getRandomColor();
+
+        int randomNumber = new Random().nextInt(101);
+
+        return new Ball(randomColor, randomNumber);
+
+    }
+
+}
